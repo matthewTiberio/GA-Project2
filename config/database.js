5dce25004efcd25cb06c9c8e6296ad4c61454e6d
Matthew Tiberio <matthewtiberio@Matthews-MacBook-Air.local>
2022-10-28 10:14:21 -0600

    Deploy bug fix to Heroku

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect(process.env.DATABASE_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-mongoose.connect("mongodb://localhost/kithkin", {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// mongoose.connect("mongodb://localhost/kithkin", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 const db = mongoose.connection;
 
